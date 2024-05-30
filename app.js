eca7c200473be9020962b34ff255467390f32134
yasser <ymeftahi@xperlean.com>
2024-05-30 14:54:26 +0100

    like

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ require("dotenv").config()
 const app=express()
 const AuthRoute = require("./routes/AuthRoutes")
 const PostRoute = require("./routes/PostRoutes")
+const LikeRoute = require("./routes/LikesRoutes")
 app.use(express.json())
 
 
 app.use("/auth" , AuthRoute)
 app.use("/posts" , PostRoute)
+app.use("/like" , LikeRoute)
+
+app.use((err,req,res,next)=>{
+    
+    res.status(400).json({
+        
+        message:err.message,
+        stack : err.stack
+    })
+})
+
 
 
 const port=process.env.APP_PORT || 8000
